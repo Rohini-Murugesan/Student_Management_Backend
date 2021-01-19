3f5e3f5b51862d269bbbb7c1aec63395e1e324ff
Rohini <rohini.m@tataelxsi.co.in>
2021-01-20 01:00:29 +0530

    Assign mentor and update mentor

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/register-mentor", async (request, response) => {
                 request.body.password = hash;
                 request.body["students"] = [];
                 request.body["ID"] = total.length + 1;
-                request.body["accountStatus"] = "inactive";
+                request.body["accountStatus"] = "active";
                 let result = await db.collection("mentors").insertOne(request.body);
                 response
                     .status(202)
@@ -78,7 +78,7 @@ app.post("/register-student", async (request, response) => {
                 request.body["mentorId"] = "unassigned";
                 request.body["mentorName"] = "unassigned";
                 request.body["ID"] = total.length + 1;
-                request.body["accountStatus"] = "inactive";
+                request.body["accountStatus"] = "active";
                 let result = await db.collection("students").insertOne(request.body);
                 response
                     .status(202)
@@ -145,50 +145,36 @@ app.post("/login", async (request, response) => {
 
 });
 
-app.post("/assign-students", async (request, response) => {
+app.post("/assign-mentor", async (request, response) => {
     try {
         let client = await mongoClient.connect(dburl);
         let db = client.db("student_management");
-        requiredKeys = ["mentorId","studentsIdList","mentorName"];
+        requiredKeys = ["mentorId", "studentId", "mentorName"];
         Keys = Object.keys(request.body)
         if (requiredKeys.every((key) => Keys.includes(key)) && (Keys.length === requiredKeys.length)) {
             let mentorId = request.body.mentorId
-            let studentsIdList = request.body.studentsIdList
+            let studentId = request.body.studentId
             let mentorName = request.body.mentorName
             let isMentorPresent = await db
-            .collection("mentors")
-            .findOne({
-                ID: mentorId
-            });
-            if(!isMentorPresent){ response.status(404).json({"msg": "Mentor not found"}) }
-            if(studentsIdList.length === 0){ response.status(404).json({"msg": "Empty students list"}) }
-            let check = studentsIdList.every( async (element) => {
-                let isPresent =  await db
+                .collection("mentors")
+                .findOne({
+                    ID: mentorId
+                });
+            console.log(isMentorPresent)
+            let isStudentPresent = await db
                 .collection("students")
                 .findOne({
-                    ID: element, mentorId: "unassigned"
+                    ID: studentId
                 });
-                console.log("isPresent",isPresent==null ? false : true)
-                return isPresent==null ? false : true
-            })
-            console.log("check",check)
-            if(check){
-                let studentsObj = {}
-                studentsIdList.forEach(async (element) => {
-                    let stdName = await db.collection("students").find({ID: element}).project({name:1}).toArray()
-                    let isPresent = await db.collection("students").findOneAndUpdate({ID: element},{$set:{mentorId: mentorId,mentorName:mentorName}})
-                    studentsObj[element] = stdName[0].name 
-                    console.log("stdName",studentsObj)
-                });
-                console.log("studentsObj",studentsObj)
-                let currStudents = await db.collection("mentors").findOne({ID:mentorId},{students:1})
-                console.log(currStudents)
-                let updateStudents = await db.collection("mentors").findOneAndUpdate({ID: mentorId},{$set:{students: studentsObj}})
-                console.log({"msg": "Success"})
-                console.log("check",check)
-                response.status(202).json({"msg": "Success"}) 
-            }else{
-                response.status(409).json({"msg": "Failed"}) //conflicts
+            if (!isMentorPresent) { response.status(404).json({ "msg": "Mentor not found" }) }
+            else if (!isStudentPresent) { response.status(404).json({ "msg": "Student not found" }) }
+            else {
+                let result = await db.collection("students").findOneAndUpdate({ ID: studentId }, { $set: { mentorId: mentorId, mentorName: mentorName } });
+                if (result.lastErrorObject.updatedExisting) {
+                    response.status(202).json({ "msg": "Student " + studentId + " assigned successfully to " + mentorName })
+                } else {
+                    response.status(406).json({ "msg": "Assignment failed" })
+                }
             }
         } else {
             response.status(406).json({
@@ -200,6 +186,50 @@ app.post("/assign-students", async (request, response) => {
         response.sendStatus(500);
     }
 });
+
+
+app.post("/update-mentor", async (request, response) => {
+    try {
+        let client = await mongoClient.connect(dburl);
+        let db = client.db("student_management");
+        requiredKeys = ["mentorId", "studentId", "mentorName"];
+        Keys = Object.keys(request.body)
+        if (requiredKeys.every((key) => Keys.includes(key)) && (Keys.length === requiredKeys.length)) {
+            let mentorId = request.body.mentorId
+            let studentId = request.body.studentId
+            let mentorName = request.body.mentorName
+            let isMentorPresent = await db
+                .collection("mentors")
+                .findOne({
+                    ID: mentorId
+                });
+            console.log(isMentorPresent)
+            let isStudentPresent = await db
+                .collection("students")
+                .findOne({
+                    ID: studentId
+                });
+            if (!isMentorPresent) { response.status(404).json({ "msg": "Mentor not found" }) }
+            else if (!isStudentPresent) { response.status(404).json({ "msg": "Student not found" }) }
+            else {
+                let result = await db.collection("students").findOneAndUpdate({ ID: studentId }, { $set: { mentorId: mentorId, mentorName: mentorName } });
+                if (result.lastErrorObject.updatedExisting) {
+                    response.status(202).json({ "msg": "Student " + studentId + " updated successfully to " + mentorName })
+                } else {
+                    response.status(406).json({ "msg": "Updation failed" })
+                }
+            }
+        } else {
+            response.status(406).json({
+                msg: "Required details not found"
+            });
+        }
+    } catch (err) {
+        console.info("ERROR : ", err);
+        response.sendStatus(500);
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Your app is running with port ${port}`);
